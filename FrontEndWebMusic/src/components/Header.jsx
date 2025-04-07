f83d25554339fc6c6e37cbb50cbd235cd24b6992
ntmcode <manh20783@gmail.com>
2025-04-08 01:38:21 +0700

    upd

--- FrontEndWebMusic/src/components/Header.jsx
+++ FrontEndWebMusic/src/components/Header.jsx
@@ -100,7 +100,7 @@ export default function Header({ onReset, onSearch }) {
           onChange={(e) => setSearch(e.target.value)}
           onKeyPress={handleKeyPress} // Thêm sự kiện nhấn Enter
           className="w-full pl-10 pr-10 py-2 bg-gray-900 text-white rounded-full focus:outline-none focus:ring-2 focus:ring-gray-600"
-          placeholder="Search songs by title..."
+          placeholder="What do you want to play?"
         />
         <button
           onClick={handleSearch}
