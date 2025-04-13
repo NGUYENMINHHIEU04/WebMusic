1e3c94937c273ce2be0a57161aa6f377c17e0dde
buihoaglong <MontestBHL@gmail.com>
2025-04-13 09:34:41 +0700

    upd
    upd

--- FrontEndWebMusic/src/index.js
+++ FrontEndWebMusic/src/index.js
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Kommunicate from "@kommunicate/kommunicate-chatbot-plugin";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  
 );
+// Kommunicate.init("257f070a023553b53076f3a6d8396e617", {
+//   automaticChatOpenOnNavigation: true,
+//   popupWidget: true
+// });
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
